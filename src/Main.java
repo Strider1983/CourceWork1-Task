56db86d714d7041b6173a9aae0e4f2c86b6ade17
Strider <mezentsev.k@gmail.com>
2023-09-18 20:57:23 +0800

    Получен реверс

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Куросовая работа. Задачи");
+        String palindrome = "Привет";
+        char[] palinDrome = palindrome.toCharArray();
+        char[] reverse = new char[palinDrome.length];
+        for (int i = palinDrome.length - 1; i > -1; i = i - 1) {
+            reverse[palinDrome.length - 1 - i] = palinDrome[i];
+        }
+        System.out.println(Arrays.toString(reverse));
+
     }
 }
