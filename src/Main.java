ed71b27016b916ec59754fac578ff6c62e59b141
Strider <mezentsev.k@gmail.com>
2023-09-18 21:23:06 +0800

    Задача выполнена

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
+import java.sql.SQLOutput;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Куросовая работа. Задачи");
-        String palindrome = "Привет";
+        System.out.println("Куросовая работа. Задача");
+        String string = "А в Енисее - синева";
+        String palindrome = string.toLowerCase();
+        palindrome = palindrome.replace(" ", "");
+        palindrome = palindrome.replace("-", "");
         char[] palinDrome = palindrome.toCharArray();
         char[] reverse = new char[palinDrome.length];
         for (int i = palinDrome.length - 1; i > -1; i = i - 1) {
             reverse[palinDrome.length - 1 - i] = palinDrome[i];
         }
-        System.out.println(Arrays.toString(reverse));
+        if (Arrays.equals(palinDrome, reverse)) {
+            System.out.println("Строка '" + string + "' - является палиндромом");
+        } else {
+            System.out.println("Строка '" + string + " - не палиндром");
+        }
 
     }
 }
